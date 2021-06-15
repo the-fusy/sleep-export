08c179fb4bb7d60a069375eae2faf66218ce081d
Nikita <the@fusy.ru>
2021-06-15 14:47:04 +0300

    Initial Commit

---
+++ sleep-export/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sleep-export
+//
+//  Created by Sukhorukov Nikita on 15.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sleep-export/sleep_exportApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleep_exportApp.swift
+//  sleep-export
+//
+//  Created by Sukhorukov Nikita on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct sleep_exportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
